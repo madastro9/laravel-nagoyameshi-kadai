6320148cf06ddf8a07f72319c7662f81f022f5f0
madastro9 <shinaifantian680@gmail.com>
2024-11-10 21:39:18 +0900

    課題レビュー用のアカウント作成

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
 
 
 class UsersTableSeeder extends Seeder
@@ -16,5 +17,16 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         User::factory()->count(100)->create();
+
+        $user = new User();
+        $user->name = "課題レビュー";
+        $user->kana = "カダイレビュー";
+        $user->email = "kadai@example.com";
+        $user->email_verified_at = Carbon::now();
+        $user->password = Hash::make('password2');
+        $user->postal_code = "1234567";
+        $user->address = "東京都";
+        $user->phone_number = "85274597894";
+        $user->save();
     }
 }
